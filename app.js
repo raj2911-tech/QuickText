603574a8757943282672882499481dc41223dbfb
raj2911-tech <raj317073b@gmail.com>
2025-07-26 13:42:21 +0530

    install EJS and create basic EJS pages for views

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-
+app.set('view engine', 'ejs');
 
 // Use router
 const snippetRoutes = require('./routes/snippet.js');
